fa6747e67d3ed73c9b0c796c36587a4e2faf94cd
sydney <sydneyosindi03@gmail.com>
2025-07-30 20:12:10 +0300

    Fix jest-dom import for CodeGrade tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,4 +1,5 @@
 import { render, screen, fireEvent, waitFor, act } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from '../App';
 
 describe('App Component', () => {
